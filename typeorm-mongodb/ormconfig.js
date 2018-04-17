299fb92d407b040e480377eb0d2bddc2295af5ea
M Haidar Hanif <git@mhaidarhanif.com>
2018-04-17 15:17:23 +0700

    Turn ormconfig.json to ormconfig.js

---
+++ typeorm-mongodb/ormconfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+  type: "mongodb",
+  database: "test",
+  synchronize: true,
+  logging: false,
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+  cli: {
+    entitiesDir: "src/entity",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscriber"
+  }
+};
---
+++ typeorm-sqlite/ormconfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+  type: "sqlite",
+  database: "database.db",
+  synchronize: true,
+  logging: false,
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+  cli: {
+    entitiesDir: "src/entity",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscriber"
+  }
+};
